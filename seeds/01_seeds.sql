f8c476b30efebed1134a70475dcf169a6e6cd6e2
PeterDevelops <PeterDinhBC@gmail.com>
2024-04-10 14:58:57 -0700

    Add query for seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (name, email, password) VALUES
+('Peter Stanley', 'peternanaimo@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Chey Meyer', 'cheychey@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Chris Parks', 'chriscross@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES
+(3, 'Hidden Door', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',
+438, 4, 4, 4, 'Canada', '6223 Wally Wally Street', 'Calgary', 'Alberta', 'KT92G7', true),
+(1, 'Lost In Time', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
+256, 2, 1, 1, 'Canada', '4478 Some Street', 'Nanaimo', 'BC', 'J7T6H8', true),
+(2, 'Tales of Mystery', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
+334, 2, 2, 3, 'Canada', '7255 Fairy Way', 'Toronto', 'Ontario', 'F3Y4K9', true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES
+('2018-09-11', '2018-09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2023-10-01', '2023-10-14', 1, 3);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES
+(3, 2, 1, 3, 'messages'),
+(2, 2, 2, 4, 'messages'),
+(3, 1, 3, 3, 'messages');
